56fbb4f7f0e1e5f874aa557cd1a88d3dc175e636
chhotu marandi <cmarandi29@gmail.com>
2023-04-18 16:49:44 +0530

    toggle function working but not in big screen

--- app.js
+++ app.js
@@ -2,14 +2,10 @@ const toggleBtn = document.querySelector(".toggle-btn");
 
 const linkSection = document.querySelector(".link");
 
-console.log(linkSection);
-
 const toggleFunction = () => {
   
 linkSection.classList.toggle("show-links");
-
-
-  
+ 
 };
 
 toggleBtn.addEventListener('click', toggleFunction);
