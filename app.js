441745c27d7134ef86f09daea7b3387150374655
chhotu marandi <cmarandi29@gmail.com>
2023-04-18 14:43:49 +0530

    added box-shadow in link section

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-alert('hi')
+const toggleBtn = document.querySelector(".toggle-btn");
+
+const linkSection = document.querySelector(".link");
+
+console.log(linkSection);
+
+const toggleFunction = () => {
+  
+linkSection.remove();
+
+
+  
+};
+
+toggleBtn.addEventListener('click', toggleFunction);
